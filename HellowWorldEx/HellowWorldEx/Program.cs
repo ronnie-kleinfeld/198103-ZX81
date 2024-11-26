fc0ead71ac6c50fef2f1c19bc4de6a0108595589
Ronnie Kleinfeld <ronnie.kleinfeld@gmail.com>
2024-11-26 21:39:14 +0200

    Update Program.cs

--- HellowWorldEx/HellowWorldEx/Program.cs
+++ HellowWorldEx/HellowWorldEx/Program.cs
@@ -1,13 +1,21 @@
-﻿namespace HellowWorldEx {
-    public class Program {
-        public async static Task Main(string[] args) {
+﻿using System.Reflection;
+
+namespace HellowWorldEx
+{
+    public class Program
+    {
+        public async static Task Main(string[] args)
+        {
             Console.WriteLine("Hello World Ex");
 
             string str;
 
-            if (args.Length > 0) {
+            if (args.Length > 0)
+            {
                 str = args[0];
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("Enter your state ment or number");
 
                 str = Console.ReadLine() ?? "";
@@ -15,28 +23,35 @@
 
             str = str.Trim();
 
-            if (int.TryParse(str, out int number)) {
+            if (int.TryParse(str, out int number))
+            {
                 await Hello(number);
-            } else {
+            }
+            else
+            {
                 str = str == string.Empty ? "Back to triangle" : str;
 
                 await Hello(str);
             }
         }
 
-        public async static Task Hello(int number) {
+        public async static Task Hello(int number)
+        {
             Console.WriteLine(number);
             Console.WriteLine();
 
-            for (int i = 0; i < number; i++) {
-                for (int j = 0; j < i + 1; j++) {
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < i + 1; j++)
+                {
                     Console.Write("*");
                 }
                 Console.WriteLine();
             }
         }
 
-        public async static Task Hello(string text) {
+        public async static Task Hello(string text)
+        {
             Console.WriteLine(text);
 
             int delay = 75;
@@ -44,13 +59,16 @@
             string alphaNumeric = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
             string str = string.Empty;
 
-            var line = 5;
+            var line = Console.CursorTop + 1;
 
-            for (int i = 0; i < text.Length; i++) {
-                if (alphaNumeric.Contains(text[i])) {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (alphaNumeric.Contains(text[i]))
+                {
                     var ascii = (text[i].ToString() == text[i].ToString().ToLower()) ? 97 : 65;
 
-                    for (int j = ascii; j < text[i] + 1; j++) {
+                    for (int j = ascii; j < text[i] + 1; j++)
+                    {
                         Console.SetCursorPosition(0, line);
                         Console.WriteLine(str + (char)j);
 
@@ -59,7 +77,9 @@
 
                     str += text[i];
                     line++;
-                } else {
+                }
+                else
+                {
                     await Task.Delay(delay);
 
                     Console.WriteLine(str + text[i]);
